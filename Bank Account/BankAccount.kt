66e8cbb7a1c05d90260ab6835cd12711b11be5ee
Eugenio8mola <32102046+Eugenio8mola@users.noreply.github.com>
2023-03-24 16:47:43 +0100

    Update BankAccount.kt

--- Bank Account/BankAccount.kt
+++ Bank Account/BankAccount.kt
@@ -1,1 +1,23 @@
+import java.util.concurrent.locks.ReentrantLock
 
+class BankAccount {
+    val lock = ReentrantLock()
+    var closed = false
+    var balance : Long = 0
+        get() = if(closed) throw IllegalStateException() else field
+
+    fun adjustBalance(amount: Long){
+        if(closed) throw IllegalStateException("Trying to access a Closed account")
+        lock.lock()
+        try {
+            if(amount<0 && balance< amount) throw IllegalArgumentException("Balance Insufficient")
+            else
+            balance += amount
+        } finally {
+            lock.unlock()
+        }
+    }
+    fun close() {
+        closed = true    //change account state
+    }
+}
