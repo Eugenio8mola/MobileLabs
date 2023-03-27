fa9f1fa2c46d89f868ade197991de6c097684f33
Eugenio8mola <32102046+Eugenio8mola@users.noreply.github.com>
2023-03-27 15:27:16 +0200

    Update Forth.kt
    4 test failing --> 3 da fixare con isWord = false

--- Forth/Forth.kt
+++ Forth/Forth.kt
@@ -1,6 +1,10 @@
 
 class InvalidException(message: String): Exception(message)
 val operations = mutableListOf<String>()
+val definitions = mutableListOf<String>()
+val temp = mutableListOf<String>()
+var isRedefined : Boolean = false
+var isWord : Boolean = false
 
 class Forth {
 
@@ -9,9 +13,7 @@ class Forth {
         val words = mutableListOf<String>()//val words = mutableListOf<String>("dup", "drop", "swap", "over","+","-","*","/")
         val emptyList = mutableListOf<String>()
         var stack = mutableListOf<Int>()
-
-
-        //var wordName = ": word-name definition;"
+        definitions.clear()
 
         for(point in line)
         {
@@ -24,9 +26,7 @@ class Forth {
             if(isWord) {
 
             }
-
              */
-
             var listOfNum = emptyList.toString().split(" ", "[" , "]")
             emptyList.clear()
             listOfNum = listOfNum.slice(1..(listOfNum.size-2))
@@ -43,14 +43,6 @@ class Forth {
             stack = toInteger(listOfNum,stack,words)
         }
 
-        /*
-        var listOfNum = emptyList.toString().split(" ", "[" , "]")
-        listOfNum = listOfNum.slice(1..(listOfNum.size-2))
-        println(listOfNum)
-
-        stack = toInteger(listOfNum,stack)
-
-         */
 
         return stack
     }
@@ -78,175 +70,260 @@ fun isNumeric(str: String): Boolean {
 
 fun toInteger(list : List<String>, stack : MutableList<Int>,words : MutableList<String>) :MutableList<Int>
 {
-    //val operations = mutableListOf<String>()
-    //"dup", "drop", "swap", "over","+","-","*","/"
-    var isWord : Boolean = false
+
+    isWord = false
+
     val newStack = mutableListOf<Int>()
     for((index,element) in list.withIndex()) {
         if(isNumeric(element))
         {
             stack.add(element.toInt())
-            //plus( list.elementAt(index).toBigInteger() )
         }
-        else
-        {
+        else {
+            var myEl = element.lowercase()
+            println("-----value of element is $myEl  ---------")
+            if (!isWord){
 
-            when (element.lowercase())
-            {
 
-                "+" -> {
-                    if(stack.size == 1) throw InvalidException("only one value on the stack")
-                    if(stack.size == 0) throw InvalidException("empty stack")
-                    else {
-                        newStack.add(stack.elementAt(0) + stack.elementAt( 1))
-                        stack.clear()
-                        stack.addAll(newStack)
-                        newStack.clear()
+                when (element.lowercase()) {
+
+                    "+" -> {
+                        if (stack.size == 1) throw InvalidException("only one value on the stack")
+                        if (stack.size == 0) throw InvalidException("empty stack")
+                        else {
+                            newStack.add(stack.elementAt(0) + stack.elementAt(1))
+                            stack.clear()
+                            stack.addAll(newStack)
+                            newStack.clear()
+                        }
                     }
-                }
-                "-"-> {
-                    if(stack.size == 1) throw InvalidException("only one value on the stack")
-                    if(stack.size == 0) throw InvalidException("empty stack")
-
-                    else{
-                        newStack.add(stack.elementAt(0) - stack.elementAt(1))
-                        stack.clear()
-                        stack.addAll(newStack)
-                        newStack.clear()
+                    "-" -> {
+                        if (stack.size == 1) throw InvalidException("only one value on the stack")
+                        if (stack.size == 0) throw InvalidException("empty stack")
+                        else {
+                            newStack.add(stack.elementAt(0) - stack.elementAt(1))
+                            stack.clear()
+                            stack.addAll(newStack)
+                            newStack.clear()
+                        }
                     }
-                }
-                "*"-> {
-                    if(stack.size == 1) throw InvalidException("only one value on the stack")
-                    if(stack.size == 0) throw InvalidException("empty stack")
-                    else {
+                    "*" -> {
+                        if (stack.size == 1) throw InvalidException("only one value on the stack")
+                        if (stack.size == 0) throw InvalidException("empty stack")
+                        else {
 
-                        newStack.add(stack.elementAt(stack.size-2) * stack.elementAt(stack.size-1))
-                        stack.clear()
-                        stack.addAll(newStack)
-                        newStack.clear()
+                            newStack.add(stack.elementAt(stack.size - 2) * stack.elementAt(stack.size - 1))
+                            stack.clear()
+                            stack.addAll(newStack)
+                            newStack.clear()
+                        }
                     }
-                }
-                "/"-> {
-                    if(stack.size == 1) throw InvalidException("only one value on the stack")
-                    if(stack.size == 0) throw InvalidException("empty stack")
-                    if(stack.elementAt(stack.size-1) == 0) throw InvalidException("divide by zero")
-                    else{
+                    "/" -> {
+                        if (stack.size == 1) throw InvalidException("only one value on the stack")
+                        if (stack.size == 0) throw InvalidException("empty stack")
+                        if (stack.elementAt(stack.size - 1) == 0) throw InvalidException("divide by zero")
+                        else {
 
-                        newStack.add(stack.elementAt(stack.size-2) / stack.elementAt(stack.size-1))
-                        stack.clear()
-                        stack.addAll(newStack)
-                        newStack.clear()
+                            newStack.add(stack.elementAt(stack.size - 2) / stack.elementAt(stack.size - 1))
+                            stack.clear()
+                            stack.addAll(newStack)
+                            newStack.clear()
+                        }
                     }
-                }
-                "dup"->
-                {
-                    if(stack.size == 0) throw InvalidException("empty stack")
-                    else{
-                        println(stack)
-                        stack.add(stack.last())
-                        println(stack)
-                        println("--using-- dup ")
-                        //stack.addAll(newStack)
-                        //newStack.clear()
-                    }
+                    "dup" -> {
+                        if (stack.size == 0) throw InvalidException("empty stack")
+                        else {
+                            println(stack)
+                            stack.add(stack.last())
+                            println(stack)
+                            println("--using-- dup ")
 
-                }
-                "drop"->
-                {
-                    if(stack.size == 0) throw InvalidException("empty stack")
-                    else{
-                        stack.removeAt(stack.size-1)
-
-
-
-                    }
-
-                }
-                "swap"->
-                {
-                    if(stack.size == 1) throw InvalidException("only one value on the stack")
-                    if(stack.size == 0) throw InvalidException("empty stack")
-                    else{
-
-                        newStack.add(stack.elementAt(((stack.size)-1)) )
-                        newStack.add(stack.elementAt(((stack.size)-2)) )
-                        stack.removeAt((stack.size-1))
-                        stack.removeAt((stack.size-1))
-                        stack.addAll(newStack)
-                        newStack.clear()
-
-
-                    }
-
-                }
-                "over"->
-                {
-                    if(stack.size == 1) throw InvalidException("only one value on the stack")
-                    if(stack.size == 0) throw InvalidException("empty stack")
-                    else{
-                        stack.add(stack.elementAt(index - 2) )
-
-                    }
-
-                }
-
-                ":"->
-                {
-                    if(isNumeric(list.elementAt(index+1))) throw InvalidException("illegal operation")
-                    else{
-
-                        words.clear()  //flush list at each definition
-                        operations.clear()
-                        println("initial list is $list")
-                        println(list.elementAt(1))
-                        val name = list.elementAt(1).lowercase()
-                        words.add("$name")
-                        //var operationList = list.mapIndexed { index,item -> index > 1 }.toList()
-                        operations.addAll(list.takeLast(list.size -2))
-                        println("operation is $operations")
-                        println("name is = $name")
-                        isWord = true
-                        println(words)
-                        return stack
-
-
-                    }
-
-                }
-                else -> {
-                    println("stack is $stack")
-                    println(words)
-                    var nonExistent :Boolean = true
-                    for (word in words){
-                        println("[$word] in $words ")
-                        if(element.equals(word))
-                        {
-                            nonExistent = false
                         }
 
                     }
-                    if(nonExistent) throw InvalidException("undefined operation")
-                    else
-                    {
-                        val wordStack =  toInteger(operations, stack,words)
-                        println("stack is $stack")
-                        return stack
+                    "drop" -> {
+                        if (stack.size == 0) throw InvalidException("empty stack")
+                        else {
+                            stack.removeAt(stack.size - 1)
 
 
+                        }
 
                     }
+                    "swap" -> {
+                        if (stack.size == 1) throw InvalidException("only one value on the stack")
+                        if (stack.size == 0) throw InvalidException("empty stack")
+                        else {
+
+                            newStack.add(stack.elementAt(((stack.size) - 1)))
+                            newStack.add(stack.elementAt(((stack.size) - 2)))
+                            stack.removeAt((stack.size - 1))
+                            stack.removeAt((stack.size - 1))
+                            stack.addAll(newStack)
+                            newStack.clear()
+
+
+                        }
+
+                    }
+                    "over" -> {
+                        if (stack.size == 1) throw InvalidException("only one value on the stack")
+                        if (stack.size == 0) throw InvalidException("empty stack")
+                        else {
+                            stack.add(stack.elementAt(index - 2))
+
+                        }
+
+                    }
+
+                    ":" -> {
+                        if (isNumeric(list.elementAt(index + 1))) throw InvalidException("illegal operation")
+                        else {
+
+                            isRedefined = false
+                            var value: String = ""
+                            //words.clear()  //flush list at each definition
+                            //operations.clear()
+                            println("initial list is $list")
+                            println(list.elementAt(1))
+                            val name = list.elementAt(1).lowercase()
+
+
+                            //set value
+                            for (i in 2..list.size - 1) {
+                                value += list.elementAt(i).lowercase()
+                                if (i != (list.size - 1)) {
+                                    value += " "
+                                }
+                                println("partial value is $value")
+
+                            }
+
+                            //redefinition of name
+                            for ((index, word) in words.withIndex()) {
+                                if (word.equals(name)) {
+                                    definitions.removeAt(index)
+                                    temp.add(value)   //push new value
+                                    temp.addAll(definitions)
+                                    definitions.clear()
+                                    definitions.addAll(temp)
+                                    temp.clear()
+                                    isRedefined = true
+
+                                }
+                            }
+                            if (!isRedefined)
+                                words.add("$name")
+
+                            //redefinition of value
+                            for ((index, word) in words.withIndex()) {
+                                if (word.equals(value)) {
+                                    value = definitions.elementAt(index)
+
+                                }
+                            }
+                            println("value to be pushed is $value")
+                            if (!isRedefined)
+                                definitions.add("$value")
+                            println("definitions is:  $definitions")
+
+                            //var operationList = list.mapIndexed { index,item -> index > 1 }.toList()
+                            //operations.addAll(list.takeLast(list.size -2))
+
+                            println("name is = $name")
+                            isWord = true
+                            println(words)
+
+                            //var tmpList = definitions.toString().split(" ", "[" , "]")
+                            //tmpList = tmpList.slice(1..(tmpList.size-3))
+                            // println("tmpList is : ${tmpList.toString()}")
+                            //definitions.clear()
+                            //definitions.addAll(tmpList)
+                            println("definit SIZE is ${definitions.size}")
+                            operations.clear()
+                            operations.addAll(definitions)
+                            println("operation is $operations")
+                            return stack   //ricevi prossima stringa
+                        }
+                    }
+                    else -> {
+                        println("stack is $stack")
+                        println(words)
+                        var nonExistent: Boolean = true
+                        for ((index, word) in words.withIndex()) {
+                            println("[$word] in $words ")
+                            if (element.lowercase().equals(word)) {
+                                nonExistent = false
+                                temp.add(definitions.elementAt(index))
+                            }
+
+                        }
+                        if (nonExistent) throw InvalidException("undefined operation")
+                        else {
+
+                            println("temp is:--> $temp")
+                            var tmpList = temp.toString().split(" ", "[", "]")
+                            tmpList = tmpList.slice(1..(tmpList.size - 2))
+                            println("tmpList is : ${tmpList}")
+
+
+
+                            operations.clear()
+                            operations.addAll(tmpList)
+                            temp.clear()
+                            val wordStack = toInteger(operations, stack, words)
+                            println("stack is $stack")
+
+
+                        }
+                    }
+
                 }
+            // end while
+
+        }else // end if
+            {
+                println("---------ENTERS HERE ---------")
+                println("stack is $stack")
+                println(words)
+                var nonExistent: Boolean = true
+                for ((index, word) in words.withIndex()) {
+                    println("[$word] in $words  and elem is: --> $myEl and $element")
+                    println("operations ---> $operations")
+                    if (element.lowercase().equals(word)) {
+                        nonExistent = false
+                        temp.add(definitions.elementAt(index))
+                    }
+
+                }
+                if (nonExistent) throw InvalidException("undefined operation")
+                else {
+
+                    println("temp is:--> $temp")
+                    var tmpList = temp.toString().split(" ", "[", "]")
+                    tmpList = tmpList.slice(1..(tmpList.size - 2))
+                    println("tmpList is : ${tmpList}")
+
+
+
+                    operations.clear()
+                    operations.addAll(tmpList)
+                    temp.clear()
+                    val wordStack = toInteger(operations, stack, words)
+                    println("stack is $stack")
+
+                }
+            }  // end ELSE
 
 
             }
-        }
-
 
     }
 
+    //operations.clear()
+
     return stack
 }
-
 /*
 fun main()
 {
@@ -269,9 +346,6 @@ fun main()
     println(name)
 
 
-
-
-
     //val evaluateString = "1 2 3 4 5"
     val evaluateString = "1 2 +"
 
@@ -283,15 +357,8 @@ fun main()
     myStack = toInteger(numList,myStack)
     println(myStack)
 
-
-
-
-
     val res = numList.elementAt(0).toBigInteger() + numList.elementAt(1).toBigInteger()
     println(res)
-
-
-
 
     var sent = "hello"
     val sentUpper= sent.uppercase()
